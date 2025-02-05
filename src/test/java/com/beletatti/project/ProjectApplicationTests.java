ed092e5dcd2c529c841fee131a272b51108670f6
Beletatti <kaiquebeletatti@gmail.com>
2025-02-05 14:13:21 -0300

    Project created

---
+++ src/test/java/com/beletatti/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beletatti.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
